b4916a56a9190eb296e39ec9a36d6cec28b81ff3
MinadukiSekina <taroufujimoto075@gmail.com>
2025-07-10 05:27:40 +0000

    test: #7 update TodoRepository tests for consistency

--- app/tests/repositories/test_todoRepository.py
+++ app/tests/repositories/test_todoRepository.py
@@ -16,41 +16,36 @@ class TestTodoRepositorySuccessCases:
     # =============================================================================
     # get_all_todos()の正常ケースのテスト
     # =============================================================================
-
-    # get_all_todos()の正常ケースのテスト用データ
-    get_all_todos_success_data: ClassVar[list[ParameterSet]] = [
-        # 登録が0件
-        pytest.param(
-            [],
-            [],
-            id="no_todos",
-        ),
-        # 登録が1件
-        pytest.param(
-            [TodoCreate(title="test", description="test", completed=False)],
-            [TodoRead(id=1, title="test", description="test", completed=False)],
-            id="one_todo",
-        ),
-        # 登録が複数件
-        pytest.param(
-            [
-                TodoCreate(title="test", description="test", completed=False),
-                TodoCreate(title="test2", description="test2", completed=False),
-                TodoCreate(title="test3", description="test3", completed=True),
-            ],
-            [
-                TodoRead(id=1, title="test", description="test", completed=False),
-                TodoRead(id=2, title="test2", description="test2", completed=False),
-                TodoRead(id=3, title="test3", description="test3", completed=True),
-            ],
-            id="multiple_todos",
-        ),
-    ]
-
-    # get_all_todos()の正常ケースのテスト
     @pytest.mark.parametrize(
         "create_test_todo_data, expected_todos",
-        get_all_todos_success_data,
+        [
+            # 登録が0件
+            pytest.param(
+                [],
+                [],
+                id="no_todos",
+            ),
+            # 登録が1件
+            pytest.param(
+                [TodoCreate(title="test", description="test", completed=False)],
+                [TodoRead(id=1, title="test", description="test", completed=False)],
+                id="one_todo",
+            ),
+            # 登録が複数件
+            pytest.param(
+                [
+                    TodoCreate(title="test", description="test", completed=False),
+                    TodoCreate(title="test2", description="test2", completed=False),
+                    TodoCreate(title="test3", description="test3", completed=True),
+                ],
+                [
+                    TodoRead(id=1, title="test", description="test", completed=False),
+                    TodoRead(id=2, title="test2", description="test2", completed=False),
+                    TodoRead(id=3, title="test3", description="test3", completed=True),
+                ],
+                id="multiple_todos",
+            ),
+        ],
         indirect=["create_test_todo_data"],
     )
     def test_get_all_todos_success_cases(
@@ -72,20 +67,25 @@ class TestTodoRepositorySuccessCases:
     # =============================================================================
     # create_todo()の正常ケースのテスト
     # =============================================================================
-
-    # create_todo()の正常ケースのテスト用データ
-    create_todo_success_data: ClassVar[list[ParameterSet]] = [
-        pytest.param(
-            TodoCreate(title="test", description="test", completed=False),
-            TodoRead(id=1, title="test", description="test", completed=False),
-            id="create_todo",
-        ),
-    ]
-
-    # create_todo()の正常ケースのテスト
     @pytest.mark.parametrize(
         "todo_create, expected_todo",
-        create_todo_success_data,
+        [
+            pytest.param(
+                TodoCreate(title="test", description="test", completed=False),
+                TodoRead(id=1, title="test", description="test", completed=False),
+                id="create_todo",
+            ),
+            pytest.param(
+                TodoCreate(title="test2", description="test2", completed=True),
+                TodoRead(id=1, title="test2", description="test2", completed=True),
+                id="create_todo_completed",
+            ),
+            pytest.param(
+                TodoCreate(title="test3", description="test3"),
+                TodoRead(id=1, title="test3", description="test3", completed=False),
+                id="create_todo_default_completed",
+            ),
+        ],
     )
     def test_create_todo_success_cases(
         self,
@@ -104,32 +104,27 @@ class TestTodoRepositorySuccessCases:
         assert todo == expected_todo
 
     # =============================================================================
-    # _get_todo_by_id()の異常ケースのテスト
-    # =============================================================================
-
-    # _get_todo_by_id()の異常ケースのテスト用データ
-    _get_todo_by_id_error_data: ClassVar[list[ParameterSet]] = [
-        pytest.param(
-            1,
-            [TodoCreate(title="test", description="test", completed=False)],
-            Todo(id=1, title="test", description="test", completed=False),
-            id="get_todo",
-        ),
-        pytest.param(
-            2,
-            [
-                TodoCreate(title="test", description="test", completed=False),
-                TodoCreate(title="test2", description="test2", completed=False),
-            ],
-            Todo(id=2, title="test2", description="test2", completed=False),
-            id="get_todo_multiple",
-        ),
-    ]
-
     # _get_todo_by_id()の正常ケースのテスト
+    # =============================================================================
     @pytest.mark.parametrize(
         "todo_id, create_test_todo_data, expected_todo",
-        _get_todo_by_id_error_data,
+        [
+            pytest.param(
+                1,
+                [TodoCreate(title="test", description="test", completed=False)],
+                Todo(id=1, title="test", description="test", completed=False),
+                id="get_todo_in_single_todo",
+            ),
+            pytest.param(
+                2,
+                [
+                    TodoCreate(title="test", description="test", completed=False),
+                    TodoCreate(title="test2", description="test2", completed=False),
+                ],
+                Todo(id=2, title="test2", description="test2", completed=False),
+                id="get_todo_in_multiple_todos",
+            ),
+        ],
         indirect=["create_test_todo_data"],
     )
     def test_get_todo_by_id_success_cases(
@@ -152,30 +147,25 @@ class TestTodoRepositorySuccessCases:
     # =============================================================================
     # get_todo()の正常ケースのテスト
     # =============================================================================
-
-    # get_todo()の正常ケースのテスト用データ
-    get_todo_success_data: ClassVar[list[ParameterSet]] = [
-        pytest.param(
-            1,
-            [TodoCreate(title="test", description="test", completed=False)],
-            TodoRead(id=1, title="test", description="test", completed=False),
-            id="get_todo",
-        ),
-        pytest.param(
-            2,
-            [
-                TodoCreate(title="test", description="test", completed=False),
-                TodoCreate(title="test2", description="test2", completed=False),
-            ],
-            TodoRead(id=2, title="test2", description="test2", completed=False),
-            id="get_todo_multiple",
-        ),
-    ]
-
-    # get_todo()の正常ケースのテスト
     @pytest.mark.parametrize(
         "todo_id, create_test_todo_data, expected_todo",
-        get_todo_success_data,
+        [
+            pytest.param(
+                1,
+                [TodoCreate(title="test", description="test", completed=False)],
+                TodoRead(id=1, title="test", description="test", completed=False),
+                id="get_todo_in_single_todo",
+            ),
+            pytest.param(
+                2,
+                [
+                    TodoCreate(title="test", description="test", completed=False),
+                    TodoCreate(title="test2", description="test2", completed=False),
+                ],
+                TodoRead(id=2, title="test2", description="test2", completed=False),
+                id="get_todo_in_multiple_todos",
+            ),
+        ],
         indirect=["create_test_todo_data"],
     )
     def test_get_todo_success_cases(
@@ -198,82 +188,77 @@ class TestTodoRepositorySuccessCases:
     # =============================================================================
     # update_todo()の正常ケースのテスト
     # =============================================================================
-
-    # update_todo()の正常ケースのテスト用データ
-    update_todo_success_data: ClassVar[list[ParameterSet]] = [
-        # タイトルのみ更新
-        pytest.param(
-            1,
-            [TodoCreate(title="test", description="test", completed=False)],
-            TodoUpdate(title="updated"),
-            TodoRead(id=1, title="updated", description="test", completed=False),
-            id="update_todo_title",
-        ),
-        # 説明のみ更新
-        pytest.param(
-            1,
-            [TodoCreate(title="test", description="test", completed=False)],
-            TodoUpdate(description="updated"),
-            TodoRead(id=1, title="test", description="updated", completed=False),
-            id="update_todo_description",
-        ),
-        # 完了状態のみ更新
-        pytest.param(
-            1,
-            [TodoCreate(title="test", description="test", completed=False)],
-            TodoUpdate(completed=True),
-            TodoRead(id=1, title="test", description="test", completed=True),
-            id="update_todo_completed",
-        ),
-        # タイトルと完了状態のみ更新
-        pytest.param(
-            1,
-            [TodoCreate(title="test", description="test", completed=False)],
-            TodoUpdate(title="updated", completed=True),
-            TodoRead(id=1, title="updated", description="test", completed=True),
-            id="update_todo_title_and_completed",
-        ),
-        # タイトルと説明のみ更新
-        pytest.param(
-            1,
-            [TodoCreate(title="test", description="test", completed=False)],
-            TodoUpdate(title="updated", description="updated"),
-            TodoRead(id=1, title="updated", description="updated", completed=False),
-            id="update_todo_title_and_description",
-        ),
-        # 説明と完了状態のみ更新
-        pytest.param(
-            1,
-            [TodoCreate(title="test", description="test", completed=False)],
-            TodoUpdate(description="updated", completed=True),
-            TodoRead(id=1, title="test", description="updated", completed=True),
-            id="update_todo_description_and_completed",
-        ),
-        # すべて更新
-        pytest.param(
-            1,
-            [TodoCreate(title="test", description="test", completed=False)],
-            TodoUpdate(title="updated", description="updated", completed=True),
-            TodoRead(id=1, title="updated", description="updated", completed=True),
-            id="update_todo_all",
-        ),
-        # 複数件ある内の１つの更新
-        pytest.param(
-            2,
-            [
-                TodoCreate(title="test", description="test", completed=False),
-                TodoCreate(title="test2", description="test2", completed=False),
-            ],
-            TodoUpdate(completed=True),
-            TodoRead(id=2, title="test2", description="test2", completed=True),
-            id="update_todo_multiple_one",
-        ),
-    ]
-
-    # update_todo()の正常ケースのテスト
     @pytest.mark.parametrize(
         "todo_id, create_test_todo_data, todo_update, expected_todo",
-        update_todo_success_data,
+        [
+            # タイトルのみ更新
+            pytest.param(
+                1,
+                [TodoCreate(title="test", description="test", completed=False)],
+                TodoUpdate(title="updated"),
+                TodoRead(id=1, title="updated", description="test", completed=False),
+                id="update_todo_title",
+            ),
+            # 説明のみ更新
+            pytest.param(
+                1,
+                [TodoCreate(title="test", description="test", completed=False)],
+                TodoUpdate(description="updated"),
+                TodoRead(id=1, title="test", description="updated", completed=False),
+                id="update_todo_description",
+            ),
+            # 完了状態のみ更新
+            pytest.param(
+                1,
+                [TodoCreate(title="test", description="test", completed=False)],
+                TodoUpdate(completed=True),
+                TodoRead(id=1, title="test", description="test", completed=True),
+                id="update_todo_completed",
+            ),
+            # タイトルと完了状態のみ更新
+            pytest.param(
+                1,
+                [TodoCreate(title="test", description="test", completed=False)],
+                TodoUpdate(title="updated", completed=True),
+                TodoRead(id=1, title="updated", description="test", completed=True),
+                id="update_todo_title_and_completed",
+            ),
+            # タイトルと説明のみ更新
+            pytest.param(
+                1,
+                [TodoCreate(title="test", description="test", completed=False)],
+                TodoUpdate(title="updated", description="updated"),
+                TodoRead(id=1, title="updated", description="updated", completed=False),
+                id="update_todo_title_and_description",
+            ),
+            # 説明と完了状態のみ更新
+            pytest.param(
+                1,
+                [TodoCreate(title="test", description="test", completed=False)],
+                TodoUpdate(description="updated", completed=True),
+                TodoRead(id=1, title="test", description="updated", completed=True),
+                id="update_todo_description_and_completed",
+            ),
+            # すべて更新
+            pytest.param(
+                1,
+                [TodoCreate(title="test", description="test", completed=False)],
+                TodoUpdate(title="updated", description="updated", completed=True),
+                TodoRead(id=1, title="updated", description="updated", completed=True),
+                id="update_todo_all",
+            ),
+            # 複数件ある内の１つの更新
+            pytest.param(
+                2,
+                [
+                    TodoCreate(title="test", description="test", completed=False),
+                    TodoCreate(title="test2", description="test2", completed=False),
+                ],
+                TodoUpdate(completed=True),
+                TodoRead(id=2, title="test2", description="test2", completed=True),
+                id="update_todo_multiple_one",
+            ),
+        ],
         indirect=["create_test_todo_data"],
     )
     def test_update_todo_success_cases(
@@ -297,30 +282,25 @@ class TestTodoRepositorySuccessCases:
     # =============================================================================
     # delete_todo()の正常ケースのテスト
     # =============================================================================
-
-    # delete_todo()の正常ケースのテスト用データ
-    delete_todo_success_data: ClassVar[list[ParameterSet]] = [
-        pytest.param(
-            1,
-            [TodoCreate(title="test", description="test", completed=False)],
-            None,
-            id="delete_todo",
-        ),
-        pytest.param(
-            2,
-            [
-                TodoCreate(title="test", description="test", completed=False),
-                TodoCreate(title="test2", description="test2", completed=False),
-            ],
-            None,
-            id="delete_todo_multiple",
-        ),
-    ]
-
-    # delete_todo()の正常ケースのテスト
     @pytest.mark.parametrize(
         "todo_id, create_test_todo_data, expected_todo",
-        delete_todo_success_data,
+        [
+            pytest.param(
+                1,
+                [TodoCreate(title="test", description="test", completed=False)],
+                None,
+                id="delete_todo_in_single_todo",
+            ),
+            pytest.param(
+                2,
+                [
+                    TodoCreate(title="test", description="test", completed=False),
+                    TodoCreate(title="test2", description="test2", completed=False),
+                ],
+                None,
+                id="delete_todo_in_multiple_todos",
+            ),
+        ],
         indirect=["create_test_todo_data"],
     )
     def test_delete_todo_success_cases(
@@ -350,6 +330,7 @@ class TestTodoRepositorySuccessCases:
 class TestTodoRepositoryErrorCases:
     """TodoRepositoryの異常ケースのテスト"""
 
+    # 異常ケース用のデータ。各ケースで使いまわすためにクラス変数に定義
     todo_error_data: ClassVar[list[ParameterSet]] = [
         pytest.param(999, "Todo with id 999 not found", id="not_found"),
         pytest.param(0, "Todo with id 0 not found", id="invalid_id"),
